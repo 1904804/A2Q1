946691f1eeb1c1cd166a7017ca4459bf5b36ba7f
Julian Milligan <56824914+1904804@users.noreply.github.com>
2021-01-31 20:59:27 +0000

    1

--- A2Q1/Program.cs
+++ A2Q1/Program.cs
@@ -10,6 +10,23 @@ namespace A2Q1
     {
         static void Main(string[] args)
         {
+            double vectorA;
+            double vectorB;
+            double vectorAngle;
+            double crossProduct;
+
+            Console.WriteLine("Please enter the value of the first vector -");
+            vectorA = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter the value of the second vector -");
+            vectorB = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter the value of the angle between the two vectors -");
+            vectorAngle = Convert.ToDouble(Console.ReadLine());
+
+            crossProduct = vectorA * vectorB * Math.Sin(vectorAngle);  //based on the forumal a * b * Sin Omega
+
+            Console.WriteLine("The cross product of the the two vectors is {0:F2}", crossProduct);
         }
     }
 }
